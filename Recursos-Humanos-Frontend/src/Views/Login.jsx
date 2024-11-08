fee5610073a30f0de6fab438cb720c64ec1f342f
cami507 <camila.orozco2@utp.edu.co>
2024-11-08 14:22:22 -0500

    Cambios Login (Iconos - ocultar contraseña)

--- Recursos-Humanos-Frontend/src/Views/Login.jsx
+++ Recursos-Humanos-Frontend/src/Views/Login.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import { FaUser, FaLock } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import './Login.css';
 import login from '../services/auth';
+import IconHR from "../Assets/Icons/IconHR.svg"
 
 const LoginForm = ({ handleLogin }) => {
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({ username: '', password: '' });
   const [error, setError] = useState('');
   const [loggedIn, setLoggedIn] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -27,6 +32,14 @@ const LoginForm = ({ handleLogin }) => {
     });
   };
 
+  const handleLogoClick = () => {
+    navigate('/landing');
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   if (loggedIn) {
     return <Navigate to='/dashboard' />;
   }
@@ -35,16 +48,21 @@ const LoginForm = ({ handleLogin }) => {
     <div>
       <div className='wrapper'>
         <form onSubmit={handleSubmit}>
-          <h1>Bienvenido a HRMinder</h1>
+          
+          <img className="iconoLogin"src={IconHR} onClick={handleLogoClick} style={{cursor: 'pointer'}}/>
+          <h1 className="bienvenida">Bienvenido a HRMinder</h1>
 
           <div className="input-box">
-            <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
             <FaUser className='icon' />
+            <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
           </div>
 
           <div className="input-box">
-            <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
             <FaLock className='icon'/>
+            <input type={showPassword ? "text" : "password"} name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+            <span className="toggle-password" onClick={togglePasswordVisibility}>
+              {showPassword ? <FaEyeSlash /> : <FaEye />}
+            </span>
           </div>
 
           {error && <p className="error-message">{error}</p>}
