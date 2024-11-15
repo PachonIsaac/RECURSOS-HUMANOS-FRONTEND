5679c950c128212c2a068d9bcedad0afd6c93da2
PachonIsaac <isaac.pachon@utp.edu.co>
2024-11-15 11:22:22 -0500

    fix: styles cardDashboard

--- Recursos-Humanos-Frontend/src/Views/Dashboard.jsx
+++ Recursos-Humanos-Frontend/src/Views/Dashboard.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import CardDashboard from '../Components/CardDashboard/CardDashboard';
-import { FaArrowsDownToPeople, FaPeopleGroup, FaIdCard } from "react-icons/fa6";
+import { FaPeopleGroup, FaIdCard } from "react-icons/fa6";
+import { BsFillPersonPlusFill } from "react-icons/bs";
 import {PublicRoutes} from '../Models'
 import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div className={styles.dashboard}>
       <div className={styles.mainContent}>
-        <CardDashboard title='Postulaciones' IconComponent={FaArrowsDownToPeople} url={`/${PublicRoutes.DASHBOARD_POSTULACIONES}`}/>
+        <CardDashboard title='Postulaciones' IconComponent={BsFillPersonPlusFill} url={`/${PublicRoutes.DASHBOARD_POSTULACIONES}`}/>
         <CardDashboard title='Personal' IconComponent={FaPeopleGroup} url={`/${PublicRoutes.DASHBOARD_PERSONAL}`}/>
-        <CardDashboard title='Postulaciones' IconComponent={FaArrowsDownToPeople} url={`/${PublicRoutes.DASHBOARD_POSTULACIONES}`}/>
+        <CardDashboard title='Puestos de trabajo' IconComponent={FaIdCard} />
+        <CardDashboard title='Postulaciones' IconComponent={BsFillPersonPlusFill} url={`/${PublicRoutes.DASHBOARD_POSTULACIONES}`}/>
         <CardDashboard title='Personal' IconComponent={FaPeopleGroup} url={`/${PublicRoutes.DASHBOARD_PERSONAL}`}/>
-        <CardDashboard title='Postulaciones' IconComponent={FaArrowsDownToPeople} url={`/${PublicRoutes.DASHBOARD_POSTULACIONES}`}/>
-        <CardDashboard title='Personal' IconComponent={FaPeopleGroup} url={`/${PublicRoutes.DASHBOARD_PERSONAL}`}/>
-        {/* <CardDashboard title='Puestos de trabajo' IconComponent={FaIdCard} /> */}
+        <CardDashboard title='Puestos de trabajo' IconComponent={FaIdCard} />
       </div>
     </div>
     
